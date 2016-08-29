b3d6ed29d57877ae70ed197817e7a944ea67ef34
Kinoshit <info@masarun.co>
2016-08-29 19:09:44 +0900

    any text can be runconized

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
+#!/usr/bin/python
+__doc__ = """{f}
+
+Usage:
+    {f} [-f  <FONT> | --font <FONT>] [<TEXT>]
+    {f} [<TEXT>]
+    {f} -h | --help
+
+Options:
+    -f, --font <FONT>   font number
+    <TEXT>   text to be image
+    -h --help           Show this screen and exit.
+""".format(f=__file__)
+
+from docopt import docopt
+
 import cv2
 import numpy as np
+from PIL import Image
 width = 1800
 height = 500
 
 def RGB2BGR(rgbcolor=(0, 0, 0)):
   return tuple(reversed(rgbcolor))
 
-def main(text):
+def main(text, fontnum=0):
   img = np.zeros((height, width, 3), np.uint8)
+#  trans = np.asarray(Image.new('RGBA', (width, height), (0, 0, 0, 0)))
+#  img2 = trans.copy()
   dot = cv2.imread('./img/block.png', 1)
 
   color = RGB2BGR((255, 255, 255))
   img[:] = color
   img2 = img.copy()
+
 
   fonts = [cv2.FONT_HERSHEY_COMPLEX,  # best
            cv2.FONT_HERSHEY_COMPLEX_SMALL,
@@ -24,7 +44,7 @@ def main(text):
            cv2.FONT_HERSHEY_TRIPLEX,
            cv2.FONT_ITALIC
            ]
-  font = fonts[0]
+  font = fonts[fontnum]
 #  line = cv2.CV_AA
   line = 8
   thickness = 12
@@ -40,6 +60,7 @@ def main(text):
         x2 = x+dot.shape[1]
         if y2 < img.shape[0] and x2 < img.shape[1]:
           img2[y:y2, x:x2] = dot
+
         
       
   img3 = cv2.resize(img2, (int(img.shape[1]*0.75), int(img.shape[0]*0.75)))
@@ -48,6 +69,9 @@ def main(text):
   cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-  text = 'RUNCO'
-  main(text)  
+  args = docopt(__doc__)
+  text = args.get('<TEXT>') or 'RUNCO'
+  font = args.get('--font') or 0
+
+  main(text, int(font)) 
   
